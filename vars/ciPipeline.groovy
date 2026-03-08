7bfaf0143f0dc7263a4a9feca8e502c9e7883508
Sravana Kumar <srkumar@consilio.com>
2026-03-08 15:24:18 +0530

    updated test stage

--- vars/ciPipeline.groovy
+++ vars/ciPipeline.groovy
@@ -153,8 +153,8 @@ def call(Map config = [:]) {
                                     returnStatus: true
                             )
 
-                            sh 'ls -R target || true'
-                            
+                            sh 'ls -R qa-tests/target || true'
+
                             if (result != 0) {
                                 echo "QA API tests failed. Rolling back..."
 
